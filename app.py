fcd9852427326e652a67e02ce914098c4554ccc0
EC2 Default User <ec2-user@ip-172-31-19-86.ec2.internal>
2023-10-15 16:58:22 +0000

    cambio en host de app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+
 import dash
 from dash import dcc
 from dash import html
@@ -251,4 +252,4 @@ def update_output_div(date, hour, proy):
 
 # Run the server
 if __name__ == "__main__":
-    app.run_server(debug=True)
+    app.run_server( host =" 0.0.0.0 ", debug=True)
